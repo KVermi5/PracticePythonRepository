0de1ed799a88580eb5883a51263c4dbf563d0f74
Kellie Vermillion <kellievermillion@gmail.com>
2023-12-02 00:22:46 -0800

    Streamlined previous code using lower function instead of or statements.

--- ZyBooks_Labs/phone_number_letters_to_numbers.py
+++ ZyBooks_Labs/phone_number_letters_to_numbers.py
@@ -1,33 +1,25 @@
-# 2 = abc
-# 3 = def
-# 4 = ghi
-# 5 = jkl
-# 6 = mno
-# 7 = pqrs
-# 8 = tuv
-# 9 = wxyz
-# 0 = +
 user_input = input('Enter phone number:\n')
+lower_input = user_input.lower()
 phone_number = ''
 
-for character in user_input:
+for character in lower_input:
     if ('0' <= character <= '9') or (character == '-'):
         phone_number += character
-    elif ('a' <= character <= 'c') or ('A' <= character <= 'C'):
+    elif 'a' <= character <= 'c':
         phone_number += '2'
-    elif ('d' <= character <= 'f') or ('D' <= character <= 'F'):
+    elif 'd' <= character <= 'f':
         phone_number += '3'
-    elif ('g' <= character <= 'i') or ('G' <= character <= 'I'):
+    elif 'g' <= character <= 'i':
         phone_number += '4'
-    elif ('j' <= character <= 'l') or ('J' <= character <= 'L'):
+    elif 'j' <= character <= 'l':
         phone_number += '5'
-    elif ('m' <= character <= 'o') or ('M' <= character <= 'O'):
+    elif 'm' <= character <= 'o':
         phone_number += '6'
-    elif ('p' <= character <= 's') or ('P' <= character <= 'S'):
+    elif 'p' <= character <= 's':
         phone_number += '7'
-    elif ('t' <= character <= 'v') or ('T' <= character <= 'V'):
+    elif 't' <= character <= 'v':
         phone_number += '8'
-    elif ('w' <= character <= 'z') or ('W' <= character <= 'Z'):
+    elif 'w' <= character <= 'z':
         phone_number += '9'
     elif character == '+':
         phone_number += '0'
